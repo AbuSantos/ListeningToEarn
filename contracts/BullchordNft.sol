b4995d27aa31e177b0200a4592f5afbef6e7633a
Abusantos <abusomwansantos@gmail.com>
2023-09-18 21:28:05 +0100

    working on the music nft

--- contracts/BullchordNft.sol
+++ contracts/BullchordNft.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifierc: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BullchordNft is ERC721 {
+contract BullchordNft is ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
-    string private baseTokenUri;
+    string private baseTokenURI;
     bool public mintEnabled = true;
     uint256 public constant i_MAX_SUPPLY = 10;
 
@@ -22,8 +22,25 @@ contract BullchordNft is ERC721 {
         string memory _symbol,
         string memory _tokenUri
     ) ERC721(_name, _symbol) {
-        baseTokenUri = _tokenUri;
+        baseTokenURI = _tokenUri;
     }
 
+    function mint() public onlyOwner {
+        require(mintEnabled, "Minting is Disabled");
+        require(
+            totalSupply() + 10 <= i_MAX_SUPPLY,
+            "Total supply is maxed out"
+        );
 
+        for (uint256 i = 0; i < 10; i++) {
+            uint256 _tokenId = totalSupply();
+            _mint(msg.sender, _tokenId);
+        }
+        mintEnabled = false;
+    }
+
+    // Override the baseURI function to return the token URI
+    function _baseURI() internal view override returns (string memory) {
+        return baseTokenURI;
+    }
 }
