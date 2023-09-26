c49899bc0efb2741fc9e3257a127ee508214d4aa
Abusantos <abusomwansantos@gmail.com>
2023-09-26 19:03:47 +0100

    bullchord market place

--- contracts/BullchordMarketPlace.sol
+++ contracts/BullchordMarketPlace.sol
@@ -3,83 +3,86 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 import "hardhat/console.sol";
 
 contract BullchordMarketPlace {
-    event MarketItemCreated(
-        uint256 indexed tokenId,
-        address seller,
-        address owner,
-        uint256 price,
-        bool sold
-    );
+   
     using Counters for Counters.Counter;
-
-    //initiate the listing price : optional
-    uint256 public listingPrice = 5000000000000000 wei;
-
-    // Array with all marketItem
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+    IERC721 public nftContract;
+    uint256 public listingFee = 0.025 ether;
+    uint256 private whitelistedListingFee = 0.0005 ether;
+    uint256 private listingProceeds;
+      // Array with all marketItem
     MarketItem[] public marketItems;
+    address[] public whiteList;
 
-    // MarketItem struct which holds all the required info
-    struct MarketItem {
-        uint _tokenId;
-        address payable owner;
-        address payable buyer;
-        address nftAddress;
-        bool sold;
-        uint256 price;
-    }
+    uint256[] public listMusicNft;
+    // uint256 listingPrice = 0.005 ether;
+    address payable owner;
 
-    //holds bidder and amount
-    struct Bid {
-        address payable from;
-        uint256 amount;
-    }
-
+    mapping(address => uint[]) public marketItemsOwner;
+    mapping(address => uint256) public proceeds;
+    mapping(address => mapping (uint256 => Listing)) public s_listings;
+    mapping(uint => bool) public isSold;
+    mapping(uint => bool) public isItemStaked;
+    mapping(uint256 => Bid[]) public userBids;
     // Mapping from auction index to user bids
     mapping(uint256 => Bid[]) public auctionBids;
 
     // Mapping from market index to a list of marketItem
     mapping(uint256 => MarketItem) public idToMarketItems;
     // Mapping from market index to a list of owned marketItem
-    mapping(address => uint[]) public marketItemsOwner;
 
-    /**
-     * @dev Guarantees msg.sender is owner of the given auction
-     * @param _marketItemId uint ID of the marketItem to validate its ownership belongs to msg.sender
-     */
-    modifier isOwner(uint _marketItemId) {
-        require(marketItems[_marketItemId].owner == msg.sender);
-        _;
+   struct MarketItem {
+        uint256 tokenId;
+        address seller;
+        address _nftContract;
+        uint256 price;
+        bool sold;
+    }
+    mapping(uint256 => MarketItem) public idToMarketItem;
+
+    struct Listing{
+        uint256 price;
+        address seller;
+       /* address creator;*/
+    }
+    struct Bid{
+        address payable from;
+        uint256 amount;
     }
 
-    /**
-     * @dev Disallow payments to this contract directly
-     */
-    fallback() external {
-        revert();
+    struct stakedListing{
+        uint256 price;
+        address seller;
     }
 
-    /**
-     * @dev Gets the total number of marketNft owned by an address
-     * @param _owner address of the owner
-     * @return uint total number of marketItems
-     */
-    function getMarketCountOfOwner(
-        address _owner
-    ) public view returns (uint256) {
-        marketItemsOwner[_owner].length;
+    modifier notListed(address _nftAddress, uint256 _tokenId){
+        Listing memory listing = s_listings[_nftAddress][_tokenId];
+            if(listing.price > 0){
+                revert("already listed");
+            }
+            _;
     }
 
-    function createMarketNft(
-        uint256 _tokenId,
-        address marketNft,
-        uint256 price
-    ) public payable {
-        require(price > 0, "Price must be higher than 0");
-        //optional
-        require(msg.value == listingPrice, "Listing price not met");
+    modifier isListed(address _nftAddress, uint256 _tokenId){
+        Listing memory listing = s_listings[_nftAddress][_tokenId];
+            if(listing.price <= 0){
+                revert("not listed");
+            }
+            _;
+    }
+  
+    modifier isOwner(address _nftContract, uint256 _tokenId, address spender){
+     nftContract = IERC721(_nftContract); //using the IERC721 interface  to access the nft
+      address ownerr = nftContract.ownerOf(_tokenId);
+      if(spender != ownerr){
+          revert("You're not the owner");
+      }
+      _;
     }
 }
