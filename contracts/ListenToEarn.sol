cb32cd66914ae27a2286f6c5c0c48cda28f32c8a
Abusantos <abusomwansantos@gmail.com>
2023-09-28 08:39:26 +0100

    updating the natspec

--- contracts/ListenToEarn.sol
+++ contracts/ListenToEarn.sol
@@ -98,16 +98,30 @@ contract ListenToEarn {
     }
 
     /**
-     * @notice registers a user
-     * @dev A registeration fee is included after we get to a 1000 users **THIS IS OPTIONAL**
-     * @dev the internal function, check and update is implemented when we meet the functions conditions
-     * @dev the threshold is set per ensure each user's listenThreshold is reduced by 50% after the initial withdrawal
+     * @dev Register a user as a member of the marketplace.
+     *
+     * This function allows users to register as members of the marketplace. Registered members
+     * gain access to certain features and benefits. A registration fee may be required for
+     * registration if the total number of registered users exceeds a threshold.
+     *
+     * Requirements:
+     * - The caller must not be already registered as a member.
+     * - If the total number of registered users exceeds a threshold (e.g., 1000), a registration
+     *   fee in tokens (`i_registerationFee`) must be transferred to the marketplace contract.
+     * - If the registration fee is required, the caller must approve the transfer of tokens to
+     *   this contract using the `transferFrom` function of the token contract.
+     * - If a registration fee is paid, it may be optionally burned to remove it from circulation.
+     *
+     * After successful registration, the caller is marked as a registered member, added to the list
+     * of users, and the total number of registered users is incremented. The function also checks
+     * and updates certain marketplace rates.
      */
     function registerUser() public {
-        //  * @param _user Address performing the registration
         require(!isUser[msg.sender], "already a registered member");
-        listeningTimeThreshold[msg.sender] == 2500 minutes;
+        // Set the listening time threshold for the user
+        listeningTimeThreshold[msg.sender] = 2500 minutes;
         if (registeredUser >= 1000) {
+            // Require the caller to transfer the registration fee in tokens
             require(
                 token.transferFrom(
                     msg.sender,
@@ -115,36 +129,71 @@ contract ListenToEarn {
                     i_registerationFee
                 )
             );
+            // Optionally, burn the registration fee tokens
             require(token.burn(i_registerationFee), "Burn failed");
         }
+
+        // Mark the caller as a registered user
         isUser[msg.sender] = true;
+
+        // Add the caller to the list of registered users
         users.push(msg.sender);
+
+        // Increment the total number of registered users
         registeredUser++;
+
+        // Check and update certain marketplace rates
         _checkAndUpdateRate();
     }
 
     /**
-     * @notice Calculates the reduction amount when users counts gets to a 1000
-     * @dev Validate the registered users count. Then calculate the currentRate, Use require statement to revert state when conditions are not met. .
+     * @dev Check and update the marketplace rate.
+     *
+     * This internal function checks the total number of registered users and, if it is a multiple
+     * of 1000, reduces the current marketplace rate. The reduction is calculated as 0.025% (25 basis
+     * points) of the current rate and is subtracted from the rate. Additionally, the function may
+     * burn the calculated reduction amount of tokens to adjust the rate.
+     *
+     * This function is called automatically during user registration to update the marketplace rate
+     * if needed.
      */
     function _checkAndUpdateRate() internal {
         if (registeredUser % 1000 == 0) {
+            // Calculate the reduction amount (0.025% of current rate)
             uint256 reduction = (currentRate * 25) / 100000;
+
+            // Subtract the reduction from the current rate
             currentRate -= reduction;
 
+            // Optionally, burn the reduction amount of tokens
             require(token.burn(reduction), "Burning failed");
         }
     }
 
     /**
-     * @notice Starts a listening session if the new week's accumulated time is less than threshold
-     * @dev Verification of listening session and Observation of previous session, state is restarted if true. Only user can start listening
+     * @dev Start a listening session for a registered user.
+     *
+     * This function allows registered users to start a listening session in the marketplace.
+     * A listening session is considered active if the user has listened to content for a certain duration.
+     *
+     * Requirements:
+     * - The caller must be a registered user.
+     * - A listening session can be started if the time elapsed since the last listening session
+     *   is greater than or equal to 7 days (604,800 seconds).
+     * - If the accumulated listening time for the user is less than their listening time threshold,
+     *   the accumulated time is reset to zero.
+     *
+     * After starting a listening session, the function updates the last listening time and
+     * the start time of the current session.
      */
     function startListening() public onlyUser {
         //7DAYS = 604,800 Seconds >= 4,838,400  1,693,784,083 1693296160 1693887000 5000000000000
+
+        // Ensure a minimum time interval has passed since the last listening session
         uint256 currentTime = block.timestamp;
         if (currentTime >= lastListeningTime[msg.sender] + weekDuration) {
             if (
+                // Check if the accumulated listening time is below the user's threshold
                 accumulatedListeningTime[msg.sender] <
                 listeningTimeThreshold[msg.sender]
             ) {
@@ -152,48 +201,86 @@ contract ListenToEarn {
             }
         }
 
+        // Update the last listening time and the start time of the current session
         lastListeningTime[msg.sender] = block.timestamp;
         listeningSessionStartTime[msg.sender] = block.timestamp;
     }
 
     /**
-     * @notice ends a listening session
-     * @dev creates a session and session duration to track the users time spent listening to a track, the session time
+     * @dev End a listening session and record accumulated listening time.
+     *
+     * This function allows registered users to end their current listening session
+     * and record the duration of their listening activity. The accumulated listening
+     * time is updated accordingly.
+     *
+     * Requirements:
+     * - The caller must be a registered user with an active listening session.
+     *
+     * Upon ending the listening session, the function calculates the session duration
+     * based on the time when the session started (`listeningSessionStartTime`) and the
+     * current block timestamp. The calculated duration is added to the user's accumulated
+     * listening time, and the start time of the current session is reset to zero.
      */
     function endListening() public onlyUser {
+        // Get the start time of the current listening session
         uint256 sessionStartTime = listeningSessionStartTime[msg.sender];
+
         if (sessionStartTime > 0) {
+            // Calculate the duration of the listening session
             uint256 sessionDuration = block.timestamp - sessionStartTime;
+
+            // Update the accumulated listening time
             accumulatedListeningTime[msg.sender] += sessionDuration;
+
+            // Reset the start time of the current session
             listeningSessionStartTime[msg.sender] = 0;
         }
-        // Reset the session start time
     }
 
     /**
-     * @notice rewards users after all conditions met
-     * @dev Users funds are forwarded,state is updated, only a registered user can earn rewards. Use require to revert state if conditions arent met
-     * @dev checks if the accumulatedListening time is greater than the listeningTime Threshold, if true, then checks for the lastime reward was sent, it its been upto a week
-     * @custom:listeningtime is calculated by subtracting the user's lastListeningTime from the current block.timestamp. This gives us the duration of the current listening session.
-     * @param _user Address of the listener
+     * @dev Reward a registered user based on accumulated listening time.
+     *
+     * This function allows registered users to receive rewards based on their accumulated
+     * listening time, provided it meets the specified threshold. Users can receive rewards
+     * once a week, and the reward amount is calculated based on the current marketplace rate
+     * and the accumulated listening time.
+     *
+     * Requirements:
+     * - The caller must be a registered user.
+     * - The user's accumulated listening time must be greater than or equal to their listening
+     *   time threshold.
+     * - Users can only withdraw rewards once a week.
+     * - After the first payout, the listening time threshold is reduced by half.
+     * - Users cannot reduce the threshold further after the first reduction.
+     * - The user's accumulated listening time is reset to zero after receiving rewards.
+     *
+     * The function calculates the reward amount based on the current marketplace rate and the
+     * accumulated listening time. It updates various user-related state variables, such as
+     * the last reward time and the threshold reduction status.
+     *
+     * The rewarded amount is forwarded to the user's address using the `_forwardRewards` function.
      */
     function rewardUser(address _user) public payable onlyUser {
+        // Check if the user's accumulated listening time meets the threshold
         require(
             accumulatedListeningTime[_user] >= listeningTimeThreshold[_user],
             "user's accumulated time less than threshold"
         );
 
-        //calculate the reward based of the accumulatedListeningTime
+        // Calculate the reward amount based on the current rate and accumulated time
         uint256 rewardAmount = currentRate * accumulatedListeningTime[_user];
         require(rewardAmount > 0, "User earned nothing");
 
-        //require(_user != address(this),"This contract cannot be rewarded.");
+        // Calculate the listening time since the last reward and reset accumulated time
         uint256 _listeningTime = block.timestamp - lastListeningTime[_user];
         accumulatedListeningTime[_user] = _listeningTime;
 
         if (!isFirstPaid[_user]) {
+            // Reduce the threshold after the first payout
+
             isFirstPaid[_user] = true; //the listeningtimethreshold is reduced after first payout
         } else {
+            // Check if it's time for the user's weekly withdrawal
             require(!isFirstReduction[_user], "threshold already reduced");
             require(
                 block.timestamp >= lastRewardTime[_user] + weekDuration,
@@ -201,11 +288,15 @@ contract ListenToEarn {
             );
         }
         if (isFirstPaid[_user] && !isFirstReduction[_user]) {
+            // Reduce the threshold by half after the first payout
             listeningTimeThreshold[_user] = listeningTimeThreshold[_user] / 2;
             isFirstReduction[_user] = true;
         }
+
+        // Reset the accumulated listening time
         accumulatedListeningTime[_user] = 0;
 
+        // Update the last reward time and forward the rewards to the user
         lastRewardTime[_user] = block.timestamp;
         _forwardRewards(_user, rewardAmount);
     }
